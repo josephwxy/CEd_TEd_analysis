694a4d75a27b772119787cb36e890aa137ca32ca
Xiangyu Wu <wxy496691875@gmail.com>
2022-07-12 02:14:05 +0800

    Add files via upload

---
+++ seqkit.sh
@@ -0,0 +1,4 @@
+seqkit grep -f CE_ID.txt CE.fasta > CE_top13.fasta
+seqkit grep -f TE_ID.txt TE.fasta > TE_top14.fasta
+seqkit grep -f FBL_CE_ID.txt FBL-CE.fasta > FBL_CE_top15.fasta
+seqkit grep -f FBL_TE_ID.txt FBL-TE.fasta > FBL_TE_top15.fasta
